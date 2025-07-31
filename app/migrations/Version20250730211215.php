85fbeb00139a888dd164935aea77c3a14dcc1f8f
irving Jacome <irving2291@gmail.com>
2025-07-30 22:49:38 -0500

    migrations

---
+++ app/migrations/Version20250730211215.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250730211215 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE request_information ADD organization_id VARCHAR(36) NOT NULL');
+        $this->addSql('ALTER TABLE request_information_status ADD organization_id VARCHAR(36) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE request_information DROP organization_id');
+        $this->addSql('ALTER TABLE request_information_status DROP organization_id');
+    }
+}
