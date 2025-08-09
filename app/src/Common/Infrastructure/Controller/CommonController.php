f49225c23d389b338a47c31ca874d99dfb35f679
irving Jacome <irving2291@gmail.com>
2025-08-09 10:56:00 -0500

    update versions migrations

--- app/src/Common/Infrastructure/Controller/CommonController.php
+++ app/src/Common/Infrastructure/Controller/CommonController.php
@@ -5,6 +5,7 @@ namespace App\Common\Infrastructure\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use \Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 class CommonController extends AbstractController
 {
@@ -12,5 +13,13 @@ class CommonController extends AbstractController
     public function healthCheck(): JsonResponse
     {
         return $this->json(['success' => true], 200);
+    }
+
+    #[Route('/env-test', name: 'health_check', methods: ['GET'])]
+    public function envTest(): JsonResponse
+    {
+        return $this->json([
+            'db' => env('DATABASE_URL'),
+        ], 200);
     }
 }
